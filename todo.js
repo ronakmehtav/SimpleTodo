05133af3c9d847f4029a2807ab3643dd92b76919
ronakmehtav <ronakmehtav@gmail.com>
2022-08-17 13:47:20 +0530

    added persistance in the todo

--- todo.js
+++ todo.js
@@ -1,3 +1,23 @@
+const containerSelectorString = '#toDos__container';
+const todoListCounter = initializeCounter();
+const Content = new Set();
+
+if (sessionStorage.getItem('todoContent')) {
+    const data = JSON.parse(sessionStorage.getItem('todoContent'));
+    if (data.data.length) {
+        data.data.forEach(item => Content.add(item));
+    }
+}
+
+document.querySelector(containerSelectorString).append(
+    ...Array.from(Content).map(item => {
+        return createElement('li', {
+            class: ['todo__ListItem'],
+            text: item,
+            id: `todo__listItem-${todoListCounter()}`,
+        });
+    })
+)
 /**
  *  Global Eventlistner
  * @param {Event} type Events which can occur on a element.
@@ -7,19 +27,19 @@
  * @param {Element=document} parent Element you want to attach the event.
  */
 function addGlobalEventListner(
-  type,
-  selector,
-  callback,
-  options,
-  parent = document
-) {
-  parent.addEventListener(
     type,
-    (e) => {
-      if (e.target.matches(selector)) callback(e);
-    },
-    options
-  );
+    selector,
+    callback,
+    options,
+    parent = document
+) {
+    parent.addEventListener(
+        type,
+        (e) => {
+            if (e.target.matches(selector)) callback(e);
+        },
+        options
+    );
 }
 
 /**
@@ -29,27 +49,27 @@ function addGlobalEventListner(
  * @returns
  */
 function createElement(type, options = {}) {
-  const element = document.createElement(type);
-  Object.entries(options).forEach(([key, value]) => {
-    key = key.toLocaleLowerCase();
-    if (key === 'class' || key === 'classname') {
-      value.forEach((value) => element.classList.add(value));
-      return;
-    }
-    if (key === 'dataset') {
-      Object.entries(value).forEach(([dataKey, dataValue]) => {
-        element.dataset[dataKey] = dataValue;
-      });
-      return;
-    }
-    if (key === 'text') {
-      element.textContent = value;
-      return;
-    }
+    const element = document.createElement(type);
+    Object.entries(options).forEach(([key, value]) => {
+        key = key.toLocaleLowerCase();
+        if (key === 'class' || key === 'classname') {
+            value.forEach((value) => element.classList.add(value));
+            return;
+        }
+        if (key === 'dataset') {
+            Object.entries(value).forEach(([dataKey, dataValue]) => {
+                element.dataset[dataKey] = dataValue;
+            });
+            return;
+        }
+        if (key === 'text') {
+            element.textContent = value;
+            return;
+        }
 
-    element.setAttribute(key, value);
-  });
-  return element;
+        element.setAttribute(key, value);
+    });
+    return element;
 }
 
 /**
@@ -58,31 +78,36 @@ function createElement(type, options = {}) {
  * @returns counter function which produces values index from 0.
  */
 function initializeCounter(indexFlag) {
-  let count = 0;
-  return function counter() {
-    return indexFlag ? ++count : count++;
-  };
+    let count = 0;
+    return function counter() {
+        return indexFlag ? ++count : count++;
+    };
 }
 
 addGlobalEventListner('keydown', '#todo__input', addTodo);
 addGlobalEventListner('click', '.todo__ListItem', removeTodo);
 
-const containerSelectorString = '#toDos__container';
-const todoListCounter = initializeCounter();
+function setStorage() {
+    return sessionStorage.setItem('todoContent', JSON.stringify({ data: Array.from(Content) }));
+}
 
 function addTodo(event) {
-  if (event.key === 'Enter' && event.target.value !== '') {
-    const textContent = event.target.value;
-    event.target.value = '';
-    const listItem = createElement('li', {
-      class: ['todo__ListItem'],
-      text: textContent,
-      id: `todo__listItem-${todoListCounter()}`,
-    });
-    document.querySelector(containerSelectorString).append(listItem);
-  }
+    if (event.key === 'Enter' && event.target.value !== '' && !Content.has(event.target.value)) {
+        const textContent = event.target.value;
+        event.target.value = '';
+        Content.add(textContent);
+        setStorage();
+        const listItem = createElement('li', {
+            class: ['todo__ListItem'],
+            text: textContent,
+            id: `todo__listItem-${todoListCounter()}`,
+        });
+        document.querySelector(containerSelectorString).append(listItem);
+    }
 }
 
 function removeTodo(event) {
-  event.target.remove();
+    Content.delete(event.target.textContent);
+    setStorage();
+    event.target.remove();
 }
